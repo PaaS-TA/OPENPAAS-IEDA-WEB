3a5a2ac808e687659e7132fa97a9c0074851c28b
jspark81 <jspark81@cloud4u.co.kr>
2015-10-27 09:47:16 +0900

    공통팝업 이벤트 수정

--- src/main/resources/static/js/commonPopup.js
+++ src/main/resources/static/js/commonPopup.js
@@ -1,25 +1,40 @@
 /**
  * 공통 confirm 팝업
  */
-function commonCongirmPop(popTitle, popContents, buttonText ){
+function commonCongirmPop( popTitle, popContents, buttonText, buttonEvent ){
+	if( !buttonEvent || buttonEvent = ''  )	buttonEvent = 'w2popup.close';
+	
 	w2popup.open({
 		title: '<b>' + popTitle + '</b>',
 		body : popContents,
-		buttons : '<a href="" class="btn btn-primary"><span>' + buttonText + '</span></a>'
+		buttons : '<button class="btn" onclick="javascript:' + buttonEvent + '();">' + buttonText + '</button> '
 	});
 }
 
 /**
- * 공통 confirm 팝업
+ * 공통 Regist 팝업
  */
-function commonRegistPop(popTitle, popContents, regBtnText, regEvent, closeBtnText, closeEvent ){
-	
-	if( !closeEvent ) closeEvent = 'w2popup.close';
+function commonRegistPop( popTitle, popContents, regBtnText, regEvent, closeBtnText, closeEvent ){
+	if( !regEvent || regEvent = ''  )	regEvent = 'w2popup.close';
+	if( !closeEvent || closeEvent = '' ) closeEvent = 'w2popup.close';
 	
 	w2popup.open({
 		title: '<b>' + popTitle + '</b>',
 		body : popContents,
-		  buttons   : '<button class="btn" onclick="javascript:'+regEvent+'();">' + regBtnText + '</button> '+
-		  '<button class="btn" onclick="javascript:'+closeEvent+'();">' + closeBtnText + '</button> '
+		buttons   : '<button class="btn" onclick="javascript:'+regEvent+'();">' + regBtnText + '</button> '+
+					'<button class="btn" onclick="javascript:'+closeEvent+'();">' + closeBtnText + '</button> '
+	});
+}
+
+/**
+ * 공통 프로세스 팝업 
+ */
+function commonProcessPop(processArray){
+	var buttons = '';
+	w2popup.open({
+		title: '<b>' + popTitle + '</b>',
+		body : popContents,
+		buttons   : '<button class="btn" onclick="javascript:'+regEvent+'();">' + regBtnText + '</button> '+
+					'<button class="btn" onclick="javascript:'+closeEvent+'();">' + closeBtnText + '</button> '
 	});
 }
