419900d899137cff1a4b75018402d4a07f51ac64
jspark81 <jspark81@cloud4u.co.kr>
2015-11-02 16:02:51 +0900

    -Task 조회 기능 구현 테스트

--- src/main/java/org/openpaas/ieda/web/information/task/TaskController.java
+++ src/main/java/org/openpaas/ieda/web/information/task/TaskController.java
@@ -3,7 +3,14 @@
  */
 package org.openpaas.ieda.web.information.task;
 
+import java.util.List;
+
+import org.openpaas.ieda.api.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,9 +21,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TaskController {
+	
+	@Autowired
+	private TaskConfigService service;
 
 	@RequestMapping(value="/information/listTaskHistory", method=RequestMethod.GET)
 	public String List() {
 		return "/information/listTaskHistory";
 	}
+	
+	@RequestMapping( value="/tasks", method =RequestMethod.GET)
+	public ResponseEntity listTaskHistory(){
+//		List<Task> taskList = "";
+		List<Task> contents = service.listTask();
+		return new ResponseEntity(HttpStatus.OK);
+	}
+
+/*	@RequestMapping( value="/tasks/{taskId}", method =RequestMethod.GET)
+	public ResponseEntity listTaskHistory(@PathVariable String taskId){
+//		List<Task> taskList = "";
+		List<Task> contents = service.listTask();
+		return new ResponseEntity(HttpStatus.OK);
+	}*/
+
 }
