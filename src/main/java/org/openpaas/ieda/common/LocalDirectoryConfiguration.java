0393d808535c1698ce21e1329d1643f51c5ff50d
Cheol-ho, Moon <chmoon93@gmail.com>
2015-12-22 11:24:36 +0900

    # 배포 설정을 줄이기 위해 application.property에서 디렉토리 설정등을 자동으로 처리하도록 함.   - 초기 실행 시 만들어지도록   - 화면별로 적용은 차주 진행

---
+++ src/main/java/org/openpaas/ieda/common/LocalDirectoryConfiguration.java
@@ -0,0 +1,75 @@
+package org.openpaas.ieda.common;
+
+import java.io.File;
+
+import org.springframework.http.HttpStatus;
+
+public class LocalDirectoryConfiguration {
+	final private static String BASE_DIR       = System.getProperty("user.home") + System.getProperty("file.separator") + ".bosh_plugin";
+	final private static String STEMCELL_DIR   = BASE_DIR + System.getProperty("file.separator") + "stemcell";
+	final private static String RELEASE_DIR    = BASE_DIR + System.getProperty("file.separator") + "release";
+	final private static String DEPLOYMENT_DIR = BASE_DIR + System.getProperty("file.separator") + "deployment";
+	final private static String SCRIPT_DIR     = BASE_DIR + System.getProperty("file.separator") + "script";
+	final private static String TEMP_DIR       = BASE_DIR + System.getProperty("file.separator") + "temp";
+	
+	public static boolean initialize() {
+		try { 
+			getStemcellDir();
+			getReleaseDir();
+			getDeploymentDir();
+			getScriptDir();
+			getTempDir();
+		} catch ( Exception e ) {
+			e.printStackTrace();
+		}
+		
+		return true;
+	}
+	
+	private static boolean checkAndMakeDirectory(String dirToCheck) {
+		boolean isOk = true;
+		try {
+			File dir = new File(dirToCheck);
+			
+			if ( !dir.isDirectory() ) 
+				dir.mkdirs();
+		} catch ( Exception e ) {
+			e.printStackTrace();
+			isOk = false;
+		}
+				
+		return isOk;
+	}
+	
+	public static String getStemcellDir() {
+		if ( !checkAndMakeDirectory(STEMCELL_DIR) )
+			throw new IEDACommonException("notfound.stemcell.local.exception", "스템셀 저장 위치 조회 중 오류가 발생했습니다.", HttpStatus.NOT_FOUND);
+
+		return STEMCELL_DIR;
+	}
+	
+	public static String getReleaseDir() {
+		if ( !checkAndMakeDirectory(RELEASE_DIR) )
+			throw new IEDACommonException("notfound.release.local.exception", "릴리즈 저장 위치 조회 중 오류가 발생했습니다.", HttpStatus.NOT_FOUND);
+		return RELEASE_DIR;
+	}
+	
+	public static String getDeploymentDir() {
+		if ( !checkAndMakeDirectory(DEPLOYMENT_DIR) )
+			throw new IEDACommonException("notfound.deployment.local.exception", "배포관련 저장 위치 조회 중 오류가 발생했습니다.", HttpStatus.NOT_FOUND);
+		return DEPLOYMENT_DIR;
+	}
+	
+	public static String getScriptDir() {
+		if ( !checkAndMakeDirectory(SCRIPT_DIR) )
+			throw new IEDACommonException("notfound.script.local.exception", "스크립트저장 위치 조회 중 오류가 발생했습니다.", HttpStatus.NOT_FOUND);
+		
+		return SCRIPT_DIR;
+	}
+	
+	public static String getTempDir() {
+		if ( !checkAndMakeDirectory(TEMP_DIR) )
+			throw new IEDACommonException("notfound.temp.local.exception", "임시 저장 위치 조회 중 오류가 발생했습니다.", HttpStatus.NOT_FOUND);
+		return TEMP_DIR;
+	}
+}
--- src/main/java/org/openpaas/ieda/web/OpenpaasIedaWebApplication.java
+++ src/main/java/org/openpaas/ieda/web/OpenpaasIedaWebApplication.java
@@ -2,19 +2,24 @@ package org.openpaas.ieda.web;
 
 import org.modelmapper.ModelMapper;
 import org.openpaas.ieda.common.IEDAConfiguration;
+import org.openpaas.ieda.common.LocalDirectoryConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 @EnableAsync
 @EnableConfigurationProperties(IEDAConfiguration.class)
 public class OpenpaasIedaWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OpenpaasIedaWebApplication.class, args);
+    	LocalDirectoryConfiguration.initialize();
+    	SpringApplication.run(OpenpaasIedaWebApplication.class, args);
     }
     
     @Bean
