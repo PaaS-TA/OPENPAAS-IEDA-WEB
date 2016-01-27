8cabd213817b340a2df258a17e751c5de593a7b6
Cheol-ho, Moon <chmoon93@gmail.com>
2016-01-27 17:41:06 +0900

    App PID 생성하도록 변경함.

--- src/main/java/org/openpaas/ieda/web/OpenpaasIedaWebApplication.java
+++ src/main/java/org/openpaas/ieda/web/OpenpaasIedaWebApplication.java
@@ -3,26 +3,24 @@ package org.openpaas.ieda.web;
 import org.modelmapper.ModelMapper;
 import org.openpaas.ieda.common.LocalDirectoryConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
-public class OpenpaasIedaWebApplication extends SpringBootServletInitializer {
+public class OpenpaasIedaWebApplication {
 	
-    @Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
- 
-    	LocalDirectoryConfiguration.initialize();
-       	return builder.sources(OpenpaasIedaWebApplication.class);
-	}
-
 	public static void main(String[] args) {
-    	LocalDirectoryConfiguration.initialize();
-    	SpringApplication.run(OpenpaasIedaWebApplication.class, args);
+		
+        LocalDirectoryConfiguration.initialize();
+
+        SpringApplication app = new SpringApplication(OpenpaasIedaWebApplication.class);
+
+        app.addListeners(new ApplicationPidFileWriter("app.pid"));
+        app.run(args);
+
     }
     
     @Bean
