9ba68ea57b78c552aa1f0e9d02f59fa64f59637e
jspark <jspark81@cloud4u.co.kr>
2016-02-12 17:55:34 +0900

    -Task 서비스 주석정리

--- src/main/java/org/openpaas/ieda/web/information/task/TaskController.java
+++ src/main/java/org/openpaas/ieda/web/information/task/TaskController.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.openpaas.ieda.api.Task;
@@ -14,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,7 +31,7 @@ public class TaskController extends BaseController {
 	private TaskService taskService;
 	@Autowired
 	private TaskAsyncService taskAsyncservice;
-
+	
 	@RequestMapping(value="/information/listTaskHistory", method=RequestMethod.GET)
 	public String List() {
 		return "/information/listTaskHistory";
@@ -58,14 +61,14 @@ public class TaskController extends BaseController {
 		return new ResponseEntity(HttpStatus.OK);
 	}*/
 	
-//	@RequestMapping( value="/task/debugLog/{id}", method =RequestMethod.GET)
-//	public void doDownloadTaskLog(
-//			@PathVariable("id") String taskId
-//			, HttpServletRequest request, HttpServletResponse response) {
-//		
-//		//taskAsyncservice.doGetTaskLogAsync(dto.getLogType(), dto.getTaskId());
-//		taskService.getDownloadDebugLogFile(taskId, request, response);
-//	}
+	@RequestMapping( value="/task/debugLog/{id}", method =RequestMethod.GET)
+	public void doDownloadTaskLog(
+			@PathVariable("id") String taskId
+			, HttpServletRequest request, HttpServletResponse response) {
+		
+		//taskAsyncservice.doGetTaskLogAsync(dto.getLogType(), dto.getTaskId());
+		taskService.getDownloadDebugLogFile(taskId, request, response);
+	}
 	
 	@MessageMapping("/task")
     @SendTo("/socket/task")
